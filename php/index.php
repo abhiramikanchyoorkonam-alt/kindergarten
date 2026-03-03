fcea133acd1ce0ee5358298d5e750e1820f268a1
abhiramikanchyoorkonam-alt <abhiramikanchyoorkonam@gmail.com>
2026-03-03 10:35:15 +0530

    eited php index

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@
 <body>
     <header>
         <div class="nav-bar">
-             <div class="logo"> <a href="index.html" class="logo-text">
+             <div class="logo"> <a href="index.php" class="logo-text">
                 <span>H</span>
                 <span>a</span>
                 <span>p</span>
@@ -25,16 +25,16 @@
                 <span></span>
             </button>
             <nav class="nav-items" id="nav-items">
-                <a href="about.html">About</a>
-                <a href="program.html">Program</a>
-                <a href="galary.html">Galary</a>
-                <a href="contact.html">Contact</a>
-                <a href="application.html"><button>Enroll</button></a>
+                <a href="about.php">About</a>
+                <a href="program.php">Program</a>
+                <a href="galary.php">Galary</a>
+                <a href="contact.php">Contact</a>
+                <a href="application.php"><button>Enroll</button></a>
                 <div class="login-dropdown">
                 <button class="login-btn-nav">Login ▾</button>
                 <div class="dropdown-content">
-                <a href="adm_login.html">Admin Login</a>
-                <a href="prnt_login.html">Parent Login</a>
+                <a href="adm_login.php">Admin Login</a>
+                <a href="prnt_login.php">Parent Login</a>
                 </div>
                 </div>
             </nav>
@@ -44,8 +44,8 @@
         <div class="hero-text">
             <h2>Where Little Mind <br> Grow Big.. </h2>
             <p>A Safe, Fun, & Loving Place For Early Learning </p>
-            <a href="application.html"><button>Enroll Now</button></a>
-            <a href="learnmor.html"><button>Learn More</button></a>
+            <a href="application.php"><button>Enroll Now</button></a>
+            <a href="learnmor.php"><button>Learn More</button></a>
         </div>
         <div class="hero-img">
             <img src="log/logo home.png" alt="logo">
@@ -116,8 +116,8 @@
     <h1>Give your Child A Better Life</h1>
     <p>Give your child the best start in life with HappyBuds Kindergarten!</p>
     <div class="quick-adm-button">
-    <a href="application.html"><button>Apply for Admission</button></a>
-    <a href="contact.html"><button>Contact Us</button></a></div>
+    <a href="application.php"><button>Apply for Admission</button></a>
+    <a href="contact.php"><button>Contact Us</button></a></div>
  </div>
     <div id="programDetails" class="program-details">
         <span class="close-btn" onclick="closeProgram()">✖</span>
@@ -134,11 +134,11 @@
         <div class="footer-links">
             <h3>Quick Links</h3>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="program.html">Programs</a></li>
-                <li><a href="galary.html">Gallery</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="program.php">Programs</a></li>
+                <li><a href="galary.php">Gallery</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </ul>
         </div>
 
